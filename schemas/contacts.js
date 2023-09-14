c0cd9deec2c9a8495c6bfb4592dfc0688b5a8762
anastasia7makar <anastasia7makar@gmail.com>
2023-09-14 16:00:16 +0300

    Hotfix schema's

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(3).required(),
   email: Joi.string().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string().min(6).required(),
 });
 
 module.exports = { addSchema };
