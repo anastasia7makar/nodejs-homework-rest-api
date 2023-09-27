7b5ca2fe4eaefb6b41236806027e13efdd61a22b
anastasia7makar <anastasia7makar@gmail.com>
2023-09-27 15:44:20 +0300

    hotfix

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,7 +3,7 @@ const { isValidObjectId } = require("mongoose");
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    next(res.status(400).json(`${contactId} is not valid id.`));
+    next(res.status(400).json({message: `${contactId} is not valid id.`}));
   }
   next();
 };
