d6ac42d17d5c5c1b8789e6edfe8f1a9faf9498ca
anastasia7makar <anastasia7makar@gmail.com>
2023-09-29 18:44:16 +0300

    refactor code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,8 +1,27 @@
 const { Contact } = require("../models/contact");
 const { ctrlWrapper } = require("../helpers");
+const { CustomErrors } = require("../helpers/customErrors");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const searchParams = {
+    owner,
+  };
+
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  if (typeof favorite === "undefined") {
+    delete searchParams.favorite;
+  } else {
+    searchParams.favorite = favorite;
+  }
+
+  const result = await Contact.find(searchParams, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
+
   res.json(result);
 };
 
@@ -10,14 +29,15 @@ const getById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findById(contactId);
   if (!result) {
-    res.status(404).json({ message: "Not found" });
-    return;
+    throw CustomErrors.NotFound();
   }
   res.json(result);
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+
   res.status(201).json(result);
 };
 
@@ -25,8 +45,7 @@ const updateContact = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndUpdate(contactId, req.body);
   if (!result) {
-    res.status(404).json({ message: "Not found" });
-    return;
+    throw CustomErrors.NotFound();
   }
   res.status(200).json(result);
 };
@@ -35,7 +54,7 @@ const removeContact = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
-    res.status(404).json({ message: "Not found" });
+    throw CustomErrors.NotFound();
   }
   res.status(200).json({
     message: "contact deleted",
@@ -44,10 +63,11 @@ const removeContact = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
-    res.status(404).json({ message: "Not found" });
-    return;
+    throw CustomErrors.NotFound();
   }
   res.status(200).json(result);
 };
@@ -57,6 +77,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   addContact: ctrlWrapper(addContact),
   updateContact: ctrlWrapper(updateContact),
-  updateStatusContact:ctrlWrapper(updateStatusContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
   removeContact: ctrlWrapper(removeContact),
 };
