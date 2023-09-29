960b89be7fd9ac6bd09c321c3a76bbc21069dc82
anastasia7makar <anastasia7makar@gmail.com>
2023-09-29 18:43:54 +0300

    add check auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
-const { isValidId, validateStatusContact, validateBody } = require("../../middlewares");
+const {
+  isValidId,
+  validateStatusContact,
+  validateBody,
+} = require("../../middlewares");
 const { schemas } = require("../../models/contact");
+const authenticate = require("../../middlewares/authenticate");
 
 const router = express.Router();
 
-router.get("/", ctrl.listContacts);
+router.get("/", authenticate, ctrl.listContacts);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
-router.delete("/:contactId", isValidId, ctrl.removeContact);
+router.delete("/:contactId", authenticate, isValidId, ctrl.removeContact);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateContact
@@ -22,6 +33,7 @@ router.put(
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateStatusContact(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
