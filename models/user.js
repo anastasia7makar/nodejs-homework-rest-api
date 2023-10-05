ef9e0038538ec6a42c3decd72e3c581e9e9ca0d7
anastasia7makar <anastasia7makar@gmail.com>
2023-10-05 17:13:33 +0300

    added avatarURL property to user schema

--- models/user.js
+++ models/user.js
@@ -24,6 +24,7 @@ const userSchema = new Schema(
       enum: ["starter", "pro", "business"],
       default: "starter",
     },
+    avatarURL: String,
     token: String,
   },
   { versionKey: false, timestamps: true }
