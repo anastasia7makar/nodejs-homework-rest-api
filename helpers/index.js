62929c2d4c7004bbe4c9d3f4d812efd82d1350bc
anastasia7makar <anastasia7makar@gmail.com>
2023-10-19 18:39:37 +0300

    added helper for sending email

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const ctrlWrapper = require("./ctrlWrappers");
 const handleMongooseError = require("./handleMongooseError");
+const sendEmail = require("./sendEmail")
 
 module.exports = {
   ctrlWrapper,
